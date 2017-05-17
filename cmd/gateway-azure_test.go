991394f9a2b8df76d7571a33c26e4fa289ff2fc5
Remco Verhoef <remco@dutchcoders.io>
2017-05-17 14:59:20 +0200

    Cleanup and update the PR with the master branch.

--- cmd/gateway-azure_test.go
+++ cmd/gateway-azure_test.go
@@ -42,8 +42,8 @@ func TestS3ToAzureHeaders(t *testing.T) {
 }
 
 func TestAzureToS3Metadata(t *testing.T) {
-	// Just one testcase. Adding more test cases does not add value to the testcase
-	// as azureToS3Metadata() just adds a prefix.
+	// Just one testcase. Adding more test cases does not add
+	// value to the testcase as azureToS3Metadata() just adds a prefix.
 	metadata := map[string]string{
 		"First-Name": "myname",
 	}
