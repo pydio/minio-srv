a78f0fabcd61fd614c964e4cdfec4ff98b1ec96d
cdujeu <charles@ajaxplorer.info>
2017-07-20 18:46:59 +0200

    Pass a fake x-pydio-user in context metadata and set as Fields to the logs

--- cmd/gateway-pydio-auth-handler.go
+++ cmd/gateway-pydio-auth-handler.go
@@ -2,12 +2,10 @@ package cmd
 
 import (
 	"net/http"
-	"context"
+	"github.com/micro/go-micro/metadata"
+	"github.com/pydio/services/common"
 )
 
-const (
-	PydioCtxUserKey = "X-Pydio-User"
-)
 // authHandler - handles all the incoming authorization headers and validates them if possible.
 type pydioAuthHandler struct {
 	handler http.Handler
@@ -22,8 +20,13 @@ func setPydioAuthHandler(h http.Handler) http.Handler {
 // handler for validating incoming authorization headers.
 func (a pydioAuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
-	initial := r.Context()
-	newContext := context.WithValue(initial, PydioCtxUserKey, "pydio-user")
+	var md map[string]string
+	var ok bool
+	if md, ok = metadata.FromContext(r.Context()); !ok {
+		md = make(map[string]string)
+	}
+	md[common.PYDIO_CONTEXT_USER_KEY] = "gateway-pydio-user"
+	newContext := metadata.NewContext(r.Context(), md)
 	newRequest := r.WithContext(newContext)
 	a.handler.ServeHTTP(w, newRequest)
 
