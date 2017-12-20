82a07751e77d3540f0da4d2b867baabcf241e55d
cdujeu <charles@ajaxplorer.info>
2017-12-20 20:02:25 +0100

    Change newrouter method, pass a RouterOptions struct instead of a predefined list of parameters.

--- cmd/gateway-pydio.go
+++ cmd/gateway-pydio.go
@@ -74,7 +74,7 @@ type pydioObjects struct {
 // newS3Gateway returns s3 gatewaylayer
 func newPydioGateway() (GatewayLayer, error) {
 
-	router := views.NewEventRouter(false, true)
+	router := views.NewStandardRouter(views.RouterOptions{WatchRegistry:true, LogReadEvents:true})
 	api := &pydioObjects{
 		Router: router,
 	}
